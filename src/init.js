26dcc1899cba1070f70fece7c36d90cab24b795b
roeyrach <93585090+roeyrach@users.noreply.github.com>
2022-12-31 14:51:17 +0200

    4th commit

--- src/init.js
+++ src/init.js
@@ -7,18 +7,22 @@ function loadProducts(products) {
 		const productImage = productDiv.appendChild(document.createElement("img"))
 		productImage.className = "product-image"
 		productImage.src = product.image_url
-		const productName = productDiv.appendChild(document.createElement("h3"))
+		const productInfo = productDiv.appendChild(document.createElement("div"))
+		productInfo.className = "product-info"
+		const productName = productInfo.appendChild(document.createElement("h3"))
 		productName.innerHTML = product.name
-		const productDesc = productDiv.appendChild(document.createElement("h6"))
+		const productDesc = productInfo.appendChild(document.createElement("h6"))
 		productDesc.innerHTML = product.description
-		const productCategory = productDiv.appendChild(document.createElement("h4"))
+		const productCategory = productInfo.appendChild(document.createElement("h5"))
 		productCategory.innerHTML = product.category
-		const productPrice = productDiv.appendChild(document.createElement("h4"))
-		productPrice.innerHTML = "Price: " + product.price + "₪"
-		const productId = productDiv.appendChild(document.createElement("h6"))
+		const productPrice = productInfo.appendChild(document.createElement("h4"))
+		productPrice.innerHTML = product.price + "₪"
+		const productId = productInfo.appendChild(document.createElement("h6"))
 		productId.innerHTML = "Id: " + product.id
 		const buttonDiv = productDiv.appendChild(document.createElement("div"))
+		buttonDiv.className = "product-btn-div"
 		const productBtn = buttonDiv.appendChild(document.createElement("button"))
+		productBtn.className = "product-btn"
 		productBtn.innerHTML = "Add"
 		const incProduct = buttonDiv.appendChild(document.createElement("span"))
 		let inc = 0
@@ -71,6 +75,32 @@ function loadProducts(products) {
 				sum += product.price
 				console.log(sum)
 				total.innerHTML = sum + "₪"
+				const deleteBtn = cartProductDiv.appendChild(
+					document.createElement("button")
+				)
+				deleteBtn.className = "product-btn"
+				deleteBtn.id = "product-delete-btn"
+				deleteBtn.innerHTML = "Delete"
+				deleteBtn.addEventListener("click", () => {
+					if (inc === 1) {
+						sum -= product.price
+						total.innerHTML = sum + "₪"
+						inc -= 1
+						cartProductTimes.innerHTML = "x" + inc
+						incProduct.innerHTML = " + " + inc
+						console.log(cartArray.length)
+						var index = cartArray.indexOf(product.id)
+						cartArray.splice(index, 1)
+						console.log(cartArray.length)
+						cartProductDiv.remove()
+					} else {
+						sum -= product.price
+						total.innerHTML = sum + "₪"
+						inc -= 1
+						cartProductTimes.innerHTML = "x" + inc
+						incProduct.innerHTML = " + " + inc
+					}
+				})
 				document.getElementById("cart").append(cartProductDiv)
 			}
 		})
